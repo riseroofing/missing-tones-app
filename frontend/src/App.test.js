bf79508b2428dc24065107ea0d8ce4ab4120aef8
Your Tony <tony@example.com>
2025-05-25 21:52:17 +0000

    update app.js to use recharts

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {
+  BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 test('renders learn react link', () => {
   render(<App />);
